f7d8b220ffe1795580578f7510516daca8097aa7
waltstone <waltstone1@gmail.com>
2014-11-21 23:53:48 +0800

    test

--- js/app.js
+++ js/app.js
@@ -19,31 +19,35 @@ window.fbAsyncInit = function() {
       //清空結果
       $($listRoot).empty();
       $('#moreBtn').addClass('hide');
+      // 臉書登入SDK
       FB.login(function(response) {
         if(response.authResponse) {
             //讀取個人信息
-            FB.api('/me?fields=name,picture,likes.limit(60)', function(response){
-              $('.user-name').text(response.name);
-              $('.user-photo').attr('src',response.picture.data.url);
-              $('#user').removeClass('hide');
-              var likes = response.likes.data;
-              var next = response.likes.paging.next;
+            FB.api( '/me?fields=name,picture,likes.limit(60)'/*填入我們要的request*/, function(response){
+              // 把資訊插入到html裡，並顯示出來
+			  $('.user-name').text(response.name);
+			  $('.user-photo').attr('src',response.picture.data.url);
+			  $('#user').removeClass('hide');
+              // ---------------
+              // 讀取 like 的列表，並儲存到 likes, 以及下一組資料的連結到 next
+			  var likes=response.likes.data;
+			  var next=response.likes.paging.next;
+              //把讀到的資料放進html
               loadPagesInfo(likes);
-              // save next request url
-              $('#moreBtn').removeClass('hide').data('next',next);
+              // save next request url to moreBtn and show it
+			  $('#moreBtn').data('next',next).removeClass('hide');
             });
         }else{
             console.log('User cancelled login or did not fully authorize.');
         }
-      }, {scope: 'user_likes'});
+      }, {scope: 'user_likes'});//拿使用者喜歡的專頁權限
       e.preventDefault();
     });
 
     $('#moreBtn').click(function(e){
       $.getJSON( $(this).data('next'), function(response){
-        loadPagesInfo(response.data);
-        var next = response.paging.next;
-        $('#moreBtn').data('next',next);
+        //更新列表資料
+		loadPagesInfo(response.data);
       })
       e.preventDefault();
     });
@@ -55,23 +59,25 @@ window.fbAsyncInit = function() {
 var loadPagesInfo = function(pages){
 
   var counter = 0, //計算現在讀完資料沒
-      current = $('<div class="current"></div>').appendTo($listRoot); //定位當前的
+      current = $('<div class="current"></div>').appendTo($listRoot); //定位當前的資料
 
   pages.forEach(function(item, index){
     //從 template 塞資料
     var $page = $(tmpl).clone();
     FB.api(item.id, function(response){
-      $page.find('.title a').attr('src', response.link).text(response.name);
-      $page.find('.likes').text(response.likes);
-      $page.find('.about').text(response.about);
-      FB.api(response.id+'/picture?type=large', function(response){
-        $page.find('img').attr('src',response.data.url);
-        $page.appendTo(current);
+      // 塞 name, about, like 數到 html 裡。
+	  $page.find('.title a').text(response.name).attr('href',response.link);
+	  $page.find('.about').text(response.about);
+	  $page.find('.likes').text(response.likes);
+      FB.api(item.id+'/picture?type=large'/*輸入圖片連結*/, function(response){
+        // 塞資料到 html 中
+		$page.find('.thumbnail img').attr('src',response.data.url);
+		$page.appendTo(current);
         counter++;
-
         // 塞完資料以後處理一下斷行
         if(counter===pages.length){
-          $( '.current div:nth-child(3n)').after('<div class="clearfix"></div>');
+          // 利用 .current div:nth-child(3n)，讓每三個page 斷行
+		  $('.current div:nth-child(3n)').after('<div class="clearfix"></div>');
           current.children('div').unwrap();
         }
       });
